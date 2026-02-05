f0bf9de464e11da11226471e20aa1deef1be0c30
riya.logistify@gmail.com <riya.logistify@gmail.com>
2026-02-05 14:12:33 +0530

    Splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clockin/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,13 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
     debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!!!!'),
-        ),
-      ),
+    routerConfig: appRouter,
     );
   }
 }
